f9c05dac1426d00a9549fef4d6437787a92dbac7
Jakub Linde <105164215+kajzerkasia@users.noreply.github.com>
2022-10-08 13:18:12 +0200

    add icons (delete and edit) to the courses

--- src/components/courses/Course.tsx
+++ src/components/courses/Course.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Box, Button, Typography} from '@mui/material';
 import {ICourse} from "../../common/api/courses";
+import ClearIcon from '@mui/icons-material/Clear';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
 
 const Course = ({name, startDate, endDate}: ICourse) => {
 
     return (
         <Box sx={{marginTop: '2vh', display: 'flex', justifyContent: 'center'}}>
+            <Button>{<ClearIcon></ClearIcon>}</Button>
             <div className="course">
                 <Typography color="secondary">{name}</Typography>
                 <small>{startDate} - {endDate}</small>
             </div>
+            <Button>{<ModeEditIcon></ModeEditIcon>}</Button>
         </Box>
     );
 };
