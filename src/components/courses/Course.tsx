d3bd9b9daf71f998d8a4601fae0c7fc2e58d5b0a
Jakub Linde <105164215+kajzerkasia@users.noreply.github.com>
2022-10-08 13:03:55 +0200

    change Button to the Typography in Course.tsx

--- src/components/courses/Course.tsx
+++ src/components/courses/Course.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Button} from '@mui/material';
+import {Box, Button, Typography} from '@mui/material';
 import {ICourse} from "../../common/api/courses";
 
 const Course = ({name, startDate, endDate}: ICourse) => {
@@ -7,7 +7,7 @@ const Course = ({name, startDate, endDate}: ICourse) => {
     return (
         <Box sx={{marginTop: '2vh', display: 'flex', justifyContent: 'center'}}>
             <div className="course">
-                <Button size="large" color="secondary">{name}</Button>
+                <Typography color="secondary">{name}</Typography>
                 <small>{startDate} - {endDate}</small>
             </div>
         </Box>
